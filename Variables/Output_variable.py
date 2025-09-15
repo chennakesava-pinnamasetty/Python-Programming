681bc60253707a3da930ff55ac6499384a13aabc
chennakesava pinnamasetty <chennakesavapinnamasetty@gmail.com>
2025-09-15 08:59:40 +0530

    Add files via upload

---
+++ Variables/Output_variable.py
@@ -0,0 +1,30 @@
+# PYTHON - Output variables
+
+# print() function is often used to output variables.
+
+# ex:
+x = "1.python is awesome"
+print(x+"\n")
+
+
+# in the print() function, you output multiple variables separated by a 'comma' and also use '+'.
+
+x = "2.python"
+y = "is"
+z = "awesome"      
+print(x,y,z+"\n")      # python is awesome
+
+
+x = "3.python"
+y = "is"
+z = "awesome"
+print(x+y+z)                 # pythonisawesome
+print(x+" "+y+" "+z+"\n")    # python is awesome
+
+# and
+
+x = "4.python"
+y = "is"
+z = "awesome"
+print(x,y)        # python is
+print(z)          # awesome
