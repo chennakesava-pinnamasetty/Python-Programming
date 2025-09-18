145e060a8a9add30e49e45bfa65bed32fe52688a
chennakesava pinnamasetty <chennakesavapinnamasetty@gmail.com>
2025-09-18 14:19:43 +0530

    String Format

---
+++ String Formating/Formating.py
@@ -0,0 +1,24 @@
+
+# 1. using + ( concatination)
+
+'''
+name = "chenna"
+age = 22
+print("My name "+name+" and my age " +str(age)+ " years old")
+'''
+                # My name chenna and my age 22 years old
+
+
+
+# 2. using % operator ( old style)
+
+'''
+name = "chenna"
+age = 22
+print("my name is %s and my age is %d"%(name,age))       # it look like c program
+
+'''
+                # my name is chenna and my age is 22
+
+
+
