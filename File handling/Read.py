32047d0bd884de7671b9ac334433ca19408eff07
chennakesava pinnamasetty <chennakesavapinnamasetty@gmail.com>
2025-10-06 11:22:21 +0530

    File Handling

---
+++ File handling/Read.py
@@ -0,0 +1,58 @@
+
+
+# 1
+               # i gave this -->     Hai this is chenna kesava and i am learning python.
+'''
+f = open("Read.txt","r")
+print(f.read())  '''      
+                           # Hai this is chenna kesava and i am learning python.
+
+
+            
+
+# 2
+
+            # i gave this -->         
+                                      #Hai this is chenna kesava and 
+                                      #i am learning python.'''
+'''
+f = open("Read.txt","r")
+print(f.read())
+
+                               Hai this is chenna kesava and
+                               i am learning python. '''
+
+
+# 3
+
+# it is also work
+'''
+with open("Read.txt","r") as f:
+
+      print(f.read()) '''
+
+
+
+
+#  NOTE :     if we give spaces in txt , it also take spaces in ouput.
+
+
+'''
+f = open("Read.txt","r")
+print(f.read())'''
+
+                       
+                       # Hai this is chenna kesava and i am learning python.
+
+
+                       # Hai this is chenna kesava and 
+                       # i am learning python.
+                       
+                       
+                       # This is reading file.
+         
+                  
+
+'''
+f = open("Read.txt","r")
+print(f.read(15))   '''           # Hai this is che
---
+++ File handling/Write.py
@@ -0,0 +1,15 @@
+
+
+
+f = open("Write.txt","w")
+f.write("Hai this is chenna")
+f.close()
+
+
+
+
+
+f = open("Write.txt","w")
+f.write("Hai this is chenna\n")
+f.write(" Iam learning python.")
+f.close()
